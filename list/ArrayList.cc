041f373420843a052d467f17e9907c31fd546acb
WarEric <wareric@163.com>
2018-05-05 11:20:37 +0800

    add arraylist header and cc files

---
+++ list/ArrayList.cc
@@ -0,0 +1,6 @@
+/*
+ * arraylist相关函数的实现
+ * by wareric@163.com
+ * 2018-05-05
+ */
+
---
+++ list/ArrayList.h
@@ -0,0 +1,40 @@
+/*
+ * 使用数组实现的ArrayList, 仅仅是对int类型的实现，这里是头文件
+ * by wareric@163.com
+ * 2018-05-04
+ */
+#ifndef ARRAYLIST_H_
+#define ARRAYLIST_H_
+#define LIST_INIT_CAPACITY 20;
+
+class ArrayList{
+	public:
+		ArrayList(unsigned int cap = LIST_INIT_CAPACITY):
+			type_size(4), length(0), capacity(cap){ p = new int[capacity];};
+		ArrayList();
+		ArrayList(const ArrayList &orig);
+		~ArrayList();
+
+		void clear();
+		bool empty();
+		unsigned length(){return length;}
+		unsigned capacity(){return capacity;}
+		int get(int i);
+		int contains(int value);
+		bool insert(int i, int value);
+		bool push_back(int value);
+		int pop_front();
+		bool del(int i);
+		void traverse();
+
+		static void merge(ArrayList &dst, const ArrayList list1&, const ArraList list2&);
+
+		ArrayList& operator=(const ArrayList &orig);
+		bool operator==(const ArrayList &orig);
+	private:
+		int *p;
+		int type_size;
+		unsigned int length;
+		unsigned int capacity;
+};
+#endif
